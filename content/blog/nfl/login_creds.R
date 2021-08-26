ff7f3b82a81f240ec35a6ff177424990538a56c4
jennysloane <45774310+jennysloane@users.noreply.github.com>
2021-08-26 22:28:19 +1000

    Delete login_creds.R

--- content/blog/nfl/login_creds.R
+++
@@ -1,9 +0,0 @@
-auth = base64("a9e5d80d-c724-4407-a01d-742fc9:MYSPORTSFEEDS")
-
-auth2 = "a9e5d80d-c724-4407-a01d-742fc9"
-
-PASS <- as.character("YDPgGg59a8gm")
-
-key <- "980081964373-rbgq6qe2ujo235kaadc16uda96n1vivb.apps.googleusercontent.com"
-
-secret <- "3Bmzu7R3JvjFMWj9w9xHJiQ6"
